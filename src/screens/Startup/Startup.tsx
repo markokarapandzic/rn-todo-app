88f5ded0617b1b7310df3b64f69d093f7fb6f96f
markokarapandzic <markoplural@gmail.com>
2023-11-19 21:17:26 +0100

    Init Commit
    Add Firebase
    
    Setup Firebase for Android and iOS
    NOTE: Used module_headers for Firebase iOS setup
    
    Remove Boilerplate Code
    
    - Remove Example and Startup boilerplate code
    - Add TS JSX support for React Native

---
+++ src/screens/Startup/Startup.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+import { useTheme } from '../../hooks';
+import { Brand } from '../../components';
+import { ApplicationScreenProps } from '../../../@types/navigation';
+
+const Startup = ({ navigation }: ApplicationScreenProps) => {
+  const { Layout, Gutters } = useTheme();
+
+  const init = async () => {
+    await new Promise(resolve =>
+      setTimeout(() => {
+        resolve(true);
+      }, 2000),
+    );
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Main' }],
+    });
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <View style={[Layout.fill, Layout.colCenter]}>
+      <Brand />
+      <ActivityIndicator size={'large'} style={[Gutters.largeVMargin]} />
+    </View>
+  );
+};
+
+export default Startup;
